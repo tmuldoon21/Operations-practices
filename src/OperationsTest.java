4826f8bfdc64fcc22c6f16c4e78df3acf759c9cc
tmuldoon21 <42975519+tmuldoon21@users.noreply.github.com>
2018-09-28 12:27:35 -0400

    Add files via upload

---
+++ src/OperationsTest.java
@@ -0,0 +1,21 @@
+
+public class OperationsTest 
+{
+
+	public static void main(String[] args) 
+	{
+		int number; //Declaration statement 
+		number = 20; //assignment statement
+		number +=5; //number = number  + 5
+		System.out.print(number);
+		number /=2;
+		System.out.println(number);
+		number-=6;
+		System.out.println(number);
+		number*=4;
+		System.out.println(number);
+		number%=2;
+		System.out.println(number);
+	}
+
+}
